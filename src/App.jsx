18bffaf93ae635f7aac8141161aa5d15a10419ed
unknown <shaicokohn@gmail.com>
2023-11-08 16:55:04 -0300

    Web design

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import './App.css'
 function App() {
 
   return (
+    <>
+    <h1 style={{fontSize: '50px'}}><span style={{color: 'violet'}}>QUE</span> <span style={{color: 'orange'}}>ELIGES</span></h1>
     <div style={{display: 'flex'}}>
-      <div className='firstOption'>
-        <h1>Primera opción</h1>
+      <div>
+        <button className='firstOption'><h1>Primera opcion aaaaaaaaaaaaaaaaaaa</h1></button>
+      </div>
+      <div style={{backgroundColor: 'white', borderRadius: '40px', height: '60px', width: '60px', marginTop: '25px'}}>
+        <h1 style={{marginTop: '10px'}}>O</h1>
       </div>
       <div>
-        <h1>O</h1>
-      </div>
-      <div className='secondOption'>
-        <h1>Segunda opción</h1>
+        <button className='secondOption'><h1>Segunda opcion bbbbbbbbbbbbbb</h1></button>
       </div>
     </div>
+    <button className='nextButton'><h2>SIGUIENTE</h2></button>
+    </>
   )
 }
 
