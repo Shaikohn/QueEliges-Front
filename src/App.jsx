2269562651351f2225b81e99121be28d9473d60a
unknown <shaicokohn@gmail.com>
2023-11-08 12:46:34 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{display: 'flex'}}>
+      <div className='firstOption'>
+        <h1>Primera opción</h1>
+      </div>
+      <div>
+        <h1>O</h1>
+      </div>
+      <div className='secondOption'>
+        <h1>Segunda opción</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
